6f63e35b12c60ba2f158408ea6595ec79e3f6496
AriJusid <48042387@est.ort.edu.ar>
2025-03-20 12:07:56 -0300

    Avanze funciones

---
+++ script.js
@@ -0,0 +1,54 @@
+var nombreIng = document.getElementById("nombre")
+var mailIng = document.getElementById("mail")
+var passIng = document.getElementById("contra")
+var contra = document.getElementById ("contra")
+var mensaje = document.getElementById("mensaje")
+var mensaje2 = document.getElementById("mensaje2")
+var mensaje3 = document.getElementById("mensaje3")
+
+
+
+function comprobarNombre() {
+    if (nombreIng.value.length < 3){
+        mensaje.innerHTML = "Debe tener al menos 3 caracteres"
+        console.log("funciona")
+    }
+    else{
+        mensaje.innerHTML = ""
+
+    }
+}
+
+function comprobarMail() {
+
+    if (!mailIng.value.includes("@")){
+        mensaje2.innerHTML = "Mail inválido"
+        console.log("funciona")
+
+    }
+    else{
+        mensaje2.innerHTML = ""
+
+    }
+    
+}
+
+function comprobarConstraseña() {
+    if (contra.value.length < 8){
+        mensaje3.innerHTML = "Debe tener al menos 8 caracteres"
+        console.log("funciona")
+    }
+    else if(){
+
+    }
+
+    else if(){
+        
+    }
+    else{
+        mensaje3.innerHTML = ""
+
+    }
+}
+
+
