00893a27dec1ecc08a34e543c17c84152d15a4d9
AriJusid <48042387@est.ort.edu.ar>
2025-03-27 12:12:06 -0300

    Avance funciones y estilos

--- script.js
+++ script.js
@@ -5,17 +5,24 @@ var contra = document.getElementById ("contra")
 var mensaje = document.getElementById("mensaje")
 var mensaje2 = document.getElementById("mensaje2")
 var mensaje3 = document.getElementById("mensaje3")
+var mensajeFin = document.getElementById("mensajeFin")
+var boton = document.getElementById("submit").disable = false
+var hayNum = false
+var completados = 0
+const regexNum = /^(?=.*\d)/
+const regexStr = /(?=.*[a-zA-Z])/
+const regexCant = /.{8,}/
 
 
 
 function comprobarNombre() {
     if (nombreIng.value.length < 3){
         mensaje.innerHTML = "Debe tener al menos 3 caracteres"
-        console.log("funciona")
+        desactivarSubmit()
     }
     else{
         mensaje.innerHTML = ""
-
+        
     }
 }
 
@@ -23,42 +30,67 @@ function comprobarMail() {
 
     if (!mailIng.value.includes("@")){
         mensaje2.innerHTML = "Mail inválido"
-        console.log("funciona")
+        desactivarSubmit()
     }
 
     else{
         mensaje2.innerHTML = ""
-
+        
     }
     
 }
 
 function comprobarConstraseña() {
-    if (contra.value.length < 8){
-        mensaje3.innerHTML = "Debe tener al menos 8 caracteres"
-        console.log("funciona1")
-    }
-    else{
-        mensaje3.innerHTML = ""
-        console.log("funciona3")
-    }
-
-    console.log(contra.value[i])
-    console.log(isNaN(contra.value[i]))
-    
-    for(var i = 0; i < contra.value.length; i++){
-        if(isNaN(contra.value[i])){
-        mensaje3.innerHTML += "Debe tener al menos 1 número"
-        console.log("funciona2 un num")
-        }
-        else{
-            mensaje3.innerHTML -= "Debe tener al menos 1 número"
-            console.log("funciona2 esta ok")
-        }
-    
-    }
-
   
+    
+if (regexCant.test(contra.value)) {
+    mensaje3a.innerHTML = ""
+    console.log("funciona3")
+  } else {
+    mensaje3a.innerHTML = "Debe tener al menos 8 caracteres"
+        console.log("funciona1")
+        desactivarSubmit()
+  }
+  if (regexNum.test(contra.value)) {
+    mensaje3b.innerHTML = ""
+    console.log("funciona3")
+  } else {
+    mensaje3b.innerHTML = "Debe tener al menos 1 número"
+        console.log("funciona1")
+        desactivarSubmit()
+  }
+  if (regexStr.test(contra.value)) {
+    mensaje3c.innerHTML = ""
+    console.log("funciona3")
+  } else {
+    mensaje3c.innerHTML = "Debe tener al menos 1 letra"
+        console.log("funciona1")
+        desactivarSubmit()
+  }
 }
 
+function confirmarContraseña(){
+    if (contra2.value != contra.value){
+        mensaje4.innerHTML = "Las contraseñas no coinciden"
+        desactivarSubmit()
+        
+    }
+    else{
+        mensaje4.innerHTML = ""
+        
+    }
+
+}
+
+function desactivarSubmit(){
+
+    boton.disable = true
+
+}
+
+function enviarForm(){
+    if (boton.disable = false){
+    mensajeFin.innerHTML="Enviado!!!"
+    }
+}
 
